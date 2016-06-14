f5fb86dad1278c502cffd65df6a36f1e06badf78
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-06-14 11:07:27 +0100

    Add classifiers and url

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup, Extension
 setup(name="java_random", version="1.0.1",
-	  description="Provides a fast implementation of the Java random number generator",
-	  author="Matthew Bradbury",
-	  license="MIT",
-      ext_modules=[Extension("java_random", ["src/java_random_module.c", "src/java_random.c"])])
+      description="Provides a fast implementation of the Java random number generator",
+      author="Matthew Bradbury",
+      license="MIT",
+      url="https://github.com/MBradbury/python_java_random",
+      ext_modules=[Extension("java_random", ["src/java_random_module.c", "src/java_random.c"])],
+      classifiers=["Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 3"]
+)
